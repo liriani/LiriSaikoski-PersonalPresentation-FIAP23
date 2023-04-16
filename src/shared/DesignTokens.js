c9f7b65a708f35724025efe47fdbbb0030119fc4
Liriani <42298016+liriani@users.noreply.github.com>
2023-04-16 18:57:00 +0200

    Add DesignTokens.js and NormalizeStyles.js

---
+++ src/shared/DesignTokens.js
@@ -0,0 +1,60 @@
+export const Colors = {
+    NEUTRAL_WHITE: '#FFF',
+    NEUTRAL_BLACK: '#252525',
+    GRAY_LIGHT: '#F9F9F9',
+    GRAY_MEDIUM: '#9C9C9C',
+    GRAY_DARK: '#676767',
+};
+
+export const FontFamilies = {
+    PRIMARY: '"Inter", "Hind", "Arial", sans-serif',
+};
+
+export const FontLetterSpacings = {
+    SMALL: '0.5px',
+    MEDIUM: '0.15px',
+    LARGE: '1.25PX',
+};
+
+export const FontLineHeights = {
+    SMALL: '120%',
+    MEDIUM: '150%',
+};
+
+export const FontSizes = {
+    ONE: '8px',
+    ONE_HALF: '12px',
+    ONE_QUARTER: '14px',
+    TWO: '16px',
+    THREE: '24px',
+    FOUR: '32px'
+};
+
+export const FontWeights = {
+    LIGHT: 200,
+    REGULAR: 400,
+    MEDIUM: 500,
+    SEMI_BOLD: 600,
+};
+
+export const Spaces = {
+    NONE: '0',
+    HALF: '4px',
+    ONE: '8px',
+    ONE_HALF: '12px',
+    TWO: '16px',
+    THREE: '24px',
+    FOUR: '32px',
+    FIVE: '40px',
+    SIX: '48px',
+    SEVEN: '56px',
+    EIGHT: '64px',
+    NINE: '72px',
+    TEN: '80px',
+};
+
+export const BorderRadius = {
+    ONE: '4px',
+    TWO: '12px',
+    THREE: '24px',
+};
