be0432317c9c5e760d518a0b5dafddfff317f70c
Liriani <42298016+liriani@users.noreply.github.com>
2023-04-16 19:17:48 +0200

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import logo from './logoLiriDark.svg';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {AboutMe} from "./screens/AboutMe";
+import {Experience} from "./screens/Experience";
+import {Hobbies} from "./screens/Hobbies";
+import {Qualifications} from "./screens/Qualifications";
 
 
 function App() {
     return (
-        <div className="App">
+        <BrowserRouter>
+        <Routes>
+            <Route path="/AboutMe/:id" element={<AboutMe/>}/>
+            <Route path="/Experience" element={<Experience/>}/>
+            <Route path="/Hobbies" element={<Hobbies/>}/>
+            <Route path="/Qualifications" element={<Qualifications/>}/>
+            <Route path="/" element={<></>}/>
+            <Route path="*" element={<>Page Not Found</>}/>
+        </Routes>
+            <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
             </header>
         </div>
+        </BrowserRouter>
+
     );
 }
 
